368b548c364172c6fb37dd35ae762f209f17d0fa
agent p <philippwaldblick@gmail.com>
2022-06-10 12:55:04 +0200

    fully working carpediem

--- src/components/dashboardComponents/CarpediemComponent.vue
+++ src/components/dashboardComponents/CarpediemComponent.vue
@@ -1,21 +1,21 @@
 <template>
     <v-container>
-        <v-card class="graph mx-auto mt-8" rounded="xl" elevation="4">
+        <v-card class="mx-auto mt-8" rounded="xl" elevation="4">
             <v-toolbar flat class="rounded-t-xl">
                 <v-toolbar-title>Carpediem</v-toolbar-title>
                 <v-toolbar-items>
                     <v-text-field transition="scroll-y-transition" variant="outlined" density="compact"
                         label="Birthdate" type="text" class="mt-9 mr-2" v-model="user.birthdate"></v-text-field>
-                    <v-select transition="scroll-y-transition" :items="Object.keys(lifeExpectancy)" variant="outlined"
-                        density="compact" label="Country" type="text" class="mt-9 mr-2" v-model="user.country">
+                    <v-select transition="scroll-y-transition" :items="countrys" variant="outlined" density="compact"
+                        label="Country" type="text" class="mt-9 mr-2" v-model="user.country">
                     </v-select>
-                    <v-select transition="scroll-y-transition" :items="Object.keys(lifeExpectancy)" variant="outlined"
-                        density="compact" label="Country" type="text" class="mt-9" v-model="user.country"></v-select>
+                    <v-select transition="scroll-y-transition" :items="['Months', 'Weeks', 'Days']" variant="outlined"
+                        density="compact" label="Mode" type="text" class="mt-9" v-model="mode"></v-select>
                 </v-toolbar-items>
             </v-toolbar>
 
             <v-card-text class="pa-8">
-                <v-row class="squares"></v-row>
+                <v-row id="squares" class="squares"></v-row>
                 <!-- added via javascript -->
             </v-card-text>
 
@@ -33,7 +33,8 @@ export default {
     name: "CarpediemComponent",
     data: () => ({
         lifeExpectancy: lifeExpectancy,
-        mode: "weeks"
+        countrys: Object.keys(lifeExpectancy),
+        mode: "Weeks"
     }),
     mounted() {
         this.renderCarpediem
@@ -41,14 +42,28 @@ export default {
     computed: {
         renderCarpediem() {
             const squares = document.querySelector('.squares');
+            let children = [].slice.call(document.getElementById("squares").getElementsByTagName("div"), 0)
+            for (let i = 0; i < children.length; i++) {
+                children[i].parentNode.removeChild(children[i]);
+            }
 
-            var start = moment(this.user.birthdate, "DD/MM/YYYY");
-            var current = moment(this.displayTime, "DD/MM/YYYY");
-            let timeCounter = current.diff(start, this.mode)
+            let itemCounter
+            let start = moment(this.user.birthdate, "DD/MM/YYYY");
+            let current = moment(this.displayTime, "DD/MM/YYYY");
+            let timeCounter = current.diff(start, this.mode.toLowerCase())
+
+            if (this.mode === "Months") {
+                itemCounter = this.lifeExpectancy[this.user.country] * 12
+            }
+            else if (this.mode === "Weeks") {
+                itemCounter = this.lifeExpectancy[this.user.country] * 52
+            }
+            else if (this.mode === "Days") {
+                itemCounter = this.lifeExpectancy[this.user.country] * 365
+            }
 
 
-
-            for (let i = 1; i < 4160; i++) {
+            for (let i = 1; i < itemCounter; i++) {
                 let level
                 if (timeCounter >= i) {
                     level = 3
@@ -56,11 +71,22 @@ export default {
                 else {
                     level = 1
                 }
-                squares.insertAdjacentHTML('beforeend', `<div class="square" data-level="${level}"></div>`);
+                squares.insertAdjacentHTML('beforeend', `<div id="square" class="square" data-level="${level}"></div>`);
             }
         },
         ...mapState(useUserAccStore, ["user"]),
         ...mapState(useTimeMachineStore, ["displayTime"])
+    },
+    watch: {
+        'mode'() {
+            this.renderCarpediem
+        }, 
+        'user.country'(){
+            this.renderCarpediem
+        },
+        'user.birthdate'(){
+            this.renderCarpediem
+        }
     }
 }
 </script>
