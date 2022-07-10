03d4744b095fcd35e3d28a7b6a28256d0f360535
agent p <philippwaldblick@gmail.com>
2022-07-10 14:59:46 +0200

    prettier

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue';
-export type LayoutKey = 'desktop' | 'mobil';
-declare module '/home/philipp/Desktop/carpediem/node_modules/nuxt/dist/pages/runtime/composables' {
-	interface PageMeta {
-		layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>;
-	}
-}
